18603df59357e5648d5ecf48dfb7f90a5f79aec2
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-12-19 10:56:13 +0530

    Added code for payroll form page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
 import './App.css';
 
+import PayrollForm from "./components/payroll-form/payroll-form";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="">
+            <PayrollForm />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
